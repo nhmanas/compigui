14633d0ce4adbb10451feaf21bd49a8132f260e5
ibrahim biten <ibrahimbiten@gmail.com>
2018-11-02 16:16:38 +0300

    kompigui gecis eklendi

--- kompi/WindowsFormsApplication3/Form1.cs
+++ kompi/WindowsFormsApplication3/Form1.cs
@@ -31,5 +31,16 @@ namespace WindowsFormsApplication3
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kompigui frm = new kompigui();
+            frm.Show();
+        }
     }
 }
