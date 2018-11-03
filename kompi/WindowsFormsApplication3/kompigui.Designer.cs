f93cbd6d8bd107711651d3a54fed2d7141ec43d8
nhmanas <nhmanas003@gmail.com>
2018-11-03 13:13:56 +0300

    drive secme bolumu ayarlaniyor ve kompi arayuzu tekrar duzenlendi

--- kompi/WindowsFormsApplication3/kompigui.Designer.cs
+++ kompi/WindowsFormsApplication3/kompigui.Designer.cs
@@ -40,8 +40,16 @@
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -56,11 +64,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 275);
+            this.button1.Location = new System.Drawing.Point(21, 250);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Go back";
+            this.button1.Text = "Logout";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -89,9 +97,9 @@
             // 
             // execute
             // 
-            this.execute.Location = new System.Drawing.Point(284, 275);
+            this.execute.Location = new System.Drawing.Point(272, 68);
             this.execute.Name = "execute";
-            this.execute.Size = new System.Drawing.Size(75, 23);
+            this.execute.Size = new System.Drawing.Size(69, 23);
             this.execute.TabIndex = 4;
             this.execute.Text = "Execute";
             this.execute.UseVisualStyleBackColor = true;
@@ -100,40 +108,40 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Location = new System.Drawing.Point(6, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(160, 13);
+            this.label2.Size = new System.Drawing.Size(187, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Destination of a file to compress:";
+            this.label2.Text = "Destination of a directory to compress:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.Location = new System.Drawing.Point(9, 32);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(347, 20);
+            this.textBox1.Size = new System.Drawing.Size(257, 20);
             this.textBox1.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Location = new System.Drawing.Point(6, 55);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(187, 13);
+            this.label3.Size = new System.Drawing.Size(160, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Destination of a directory to compress:";
+            this.label3.Text = "Destination of a file to compress:";
             // 
             // dirdes
             // 
-            this.dirdes.Location = new System.Drawing.Point(12, 107);
+            this.dirdes.Location = new System.Drawing.Point(9, 71);
             this.dirdes.Name = "dirdes";
-            this.dirdes.Size = new System.Drawing.Size(347, 20);
+            this.dirdes.Size = new System.Drawing.Size(257, 20);
             this.dirdes.TabIndex = 8;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(284, 148);
+            this.button2.Location = new System.Drawing.Point(272, 70);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(69, 23);
             this.button2.TabIndex = 9;
             this.button2.Text = "Apply";
             this.button2.UseVisualStyleBackColor = true;
@@ -150,34 +158,122 @@
             "Once in 10 hours",
             "Twice a day",
             "Once a day"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 150);
+            this.comboBox1.Location = new System.Drawing.Point(9, 72);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(266, 21);
+            this.comboBox1.Size = new System.Drawing.Size(257, 21);
             this.comboBox1.TabIndex = 10;
             this.comboBox1.Text = "Select time interval";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 134);
+            this.label4.Location = new System.Drawing.Point(6, 56);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(101, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Timed compression:";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(272, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Execute";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(272, 30);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Apply";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] {
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "J",
+            "K",
+            "L",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "Y",
+            "Z"});
+            this.comboBox7.Location = new System.Drawing.Point(9, 32);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(257, 21);
+            this.comboBox7.TabIndex = 14;
+            this.comboBox7.Text = "Select disk";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Compress whole disk:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dirdes);
+            this.groupBox1.Controls.Add(this.execute);
+            this.groupBox1.Location = new System.Drawing.Point(12, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Manual Compression";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.comboBox7);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(347, 103);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Automatic Compression";
+            // 
             // kompigui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(371, 310);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.dirdes);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.execute);
+            this.ClientSize = new System.Drawing.Size(371, 281);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
@@ -189,6 +285,10 @@
             this.Load += new System.EventHandler(this.kompigui_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -208,5 +308,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
--- kompi/WindowsFormsApplication3/kompigui.cs
+++ kompi/WindowsFormsApplication3/kompigui.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication3
@@ -38,13 +39,43 @@ namespace WindowsFormsApplication3
             string destination = textBox1.Text;
             strCmdText = "/C compact /c /s /a /i /exe:lzx '" + destination + "'";
             System.Diagnostics.Process.Start("CMD.exe", strCmdText);
-            strCmdText = "/C compact /c /s /a /i /exe:lzx '" + dirdes + " *'";
-            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
         }
 
         private void kompigui_Load(object sender, EventArgs e)
         {
             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            string dirdes1 = dirdes.Text;
+            strCmdText = "/C compact /c /s /a /i /exe:lzx '" + dirdes1 + " *'";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = System.IO.DriveInfo.GetDrives();
+            for (int i = 1; i < 7; i++)
+            {
+                comboBox7.Items.Add(allDrives[i]);
+            }
         }
     }
 }
